9ed8414c5dff03e43d317f358677b3b1a3b2c5c2
Stefan Dentro <sd11@sanger.ac.uk>
2016-11-14 14:43:48 +0000

    Various additional columns

--- create_summary_table.R
+++ create_summary_table.R
@@ -30,10 +30,13 @@ FRAC_SNVS_CLUSTER = 0.01
 CLONAL_PEAK_MIN_CCF = 0.9
 CLONAL_PEAK_MAX_CCF = 1.1
 PLOIDY_MAX_DIPLOID = 2.7
+MEDIUM_SIZE_ABERRATION = 10 # CNA size in MB
+LARGE_SIZE_ABERRATION = 50 # CNA size in MB
 COVERAGE_FILES = c("../coverage/coverage_santa_cruz.txt", "../coverage/coverage_august_release_single.txt", "../coverage/coverage_august_release_multiple.txt", "../coverage/missing_coverage.tsv")
 #GENDER_FILES = c("../gender/2015_05_15_santa_cruz_pilot_inferred_genders.txt", "../gender/2015_08_31_santa_cruz_pilot_inferred_genders_multiplesamples.txt", "../gender/2015_10_06_august_release_genders_single.txt", "../gender/2015_10_27_august_release_genders_multiple.txt", "../gender/2016_02_01_gender_unreleased_samples.txt", "../gender/2016_03_06_october_release_genders.txt")
 GENDER_FILES = c("../gender/genders.txt")
 ICGC_HISTOLOGY_FILE = "/nfs/users/nfs_c/cgppipe/pancancer/workspace/sd11/icgc_pancan_full/clinical_data/pcawg_specimen_histology_August2016_v6_fixed.tsv"
+ICGC_CLINICAL_FILE = "/nfs/users/nfs_c/cgppipe/pancancer/workspace/sd11/icgc_pancan_full/clinical_data/pcawg_donor_clinical_August2016_v6_fixed.tsv"
 
 #samplelist = read.table("2015_10_15_icgc_samples_pass.tsv", header=T, stringsAsFactors=F)
 #samplelist = read.table("2015_10_29_icgc_samples_pass.tsv", header=T, stringsAsFactors=F)
@@ -112,12 +115,14 @@ getSubclonesAndAssignments = function(samplename, min_clonal_ccf=CLONAL_PEAK_MIN
   num_subclones = 0
   num_superclones = 0
   num_superclonal = 0
-  for (cluster in kept_clusters) {
+  cluster_locations = c()
+  cluster_sizes = c()
+  for (cluster in rev(kept_clusters)) {
     if (clusters[clusters$cluster.no==cluster,]$location > min_clonal_ccf) {
 	   # Clonal
 	   num_clonal = num_clonal + assignments[cluster]
 
-    	   if (clusters[clusters$cluster.no==cluster,]$location > max_clonal_ccf) {
+	   if (clusters[clusters$cluster.no==cluster,]$location > max_clonal_ccf) {
 		   # Superclonal
 		   num_superclones = num_superclones + 1
 		   num_superclonal = num_superclonal + assignments[cluster]
@@ -127,11 +132,18 @@ getSubclonesAndAssignments = function(samplename, min_clonal_ccf=CLONAL_PEAK_MIN
       num_subclonal = num_subclonal + assignments[cluster]
       num_subclones = num_subclones + 1
     }
+    cluster_locations = c(cluster_locations, clusters[clusters$cluster.no==cluster,]$location)
+    cluster_sizes = c(cluster_sizes, assignments[cluster])
   }
-  return(list(num_subclones, num_clonal, num_subclonal, num_superclones, num_superclonal))
+  return(list(clust_stats=list(num_subclones, num_clonal, num_subclonal, num_superclones, num_superclonal), clust_details=list(cluster_locations, cluster_sizes)))
 }
 print("Num subclones")
-res = as.data.frame(matrix(unlist(lapply(samplelist, getSubclonesAndAssignments)), ncol=5, byrow=T))
+res = lapply(samplelist, getSubclonesAndAssignments)
+clust_stats = lapply(res, function(x) x$clust_stats)
+# Saving these for later in the script to be appended to the table
+clust_details = lapply(res, function(x) x$clust_details)
+
+res = as.data.frame(matrix(unlist(clust_stats), ncol=5, byrow=T))
 colnames(res) = c("num_subclones", "num_clonal", "num_subclonal", "num_superclones", "num_superclonal")
 res$frac_clonal = round(res$num_clonal / (res$num_subclonal+res$num_clonal), 3)
 output = data.frame(output, res)
@@ -159,47 +171,71 @@ getCNstatus = function(segment, normalCN=1) {
   return(status)
 }
 
-getCNAFractions = function(samplename, data_table, max_ploid_diploid=PLOIDY_MAX_DIPLOID) {
+getCNAFractions = function(samplename, data_table, max_ploid_diploid=PLOIDY_MAX_DIPLOID, medium_size_aberration=MEDIUM_SIZE_ABERRATION, large_size_aberration=LARGE_SIZE_ABERRATION) {
   ploidy = data_table[data_table$samplename==samplename,]$ploidy
   cancer_type = data_table[data_table$samplename==samplename,]$projectcode
   subclones = read.table(paste(PATH_TO_BB, cancer_type, "/", samplename, "/", samplename, SUBCLONES_SUFFIX, sep=""), header=T, stringsAsFactors=F)
-  subclones$len = round((subclones$endpos-subclones$startpos)/1000)
+  subclones$len = round((subclones$endpos-subclones$startpos)/1000000)
   genome_len = sum(as.numeric(subclones$len))
 
   # Use ploidy to determine if there has been a WGD
   # If there is we assume 2 is the normal CN state and reason from there
   cn_sample = list(noCNA=0, subclonal=0, clonal=0)
   ploid = ifelse(ploidy<max_ploid_diploid, "diploid", "tetraploid")
+  # Check if diploid or tetraploid
+  if (ploidy <= max_ploid_diploid) {
+    # diploid
+    normalCN = 1
+  } else {
+    # tetraploid
+    normalCN = 2
+  }
+  
   chroms = unique(subclones$chr)
   for (chrom in chroms) {
     subclones.c = subclones[subclones$chr==chrom,]
     for (i in 1:nrow(subclones.c)) {
-      # Check if diploid or tetraploid
-      if (ploidy <= max_ploid_diploid) {
-        # diploid
-        status = getCNstatus(subclones.c[i,], 1)
-      } else {
-        # tetraploid
-        status = getCNstatus(subclones.c[i,], 2)
-      }
+      status = getCNstatus(subclones.c[i,], normalCN)
 
       # Add the fraction of the genome of this segment to the total of the category determined right above (status variable)
       frac = subclones.c[i,]$len
       cn_sample[names(cn_sample)==status][[1]] = cn_sample[names(cn_sample)==status][[1]] + frac
     }
   }
+  
+  has_clonal_cna = F
+  has_clonal_cna_medium = F
+  has_clonal_cna_large = F
+  if (any(subclones$frac1_A==1 & !(subclones$nMaj1_A==normalCN & subclones$nMaj1_A==subclones$nMin1_A))) {
+    has_clonal_cna = T
+  }
+  
+  if (any(subclones$frac1_A==1 & !(subclones$nMaj1_A==normalCN & subclones$nMaj1_A==subclones$nMin1_A) & subclones$len > medium_size_aberration)) {
+    has_clonal_cna_medium = T
+  }
+  
+  if (any(subclones$frac1_A==1 & !(subclones$nMaj1_A==normalCN & subclones$nMaj1_A==subclones$nMin1_A) & subclones$len > large_size_aberration)) {
+    has_clonal_cna_large = T
+  }
 
   return(list(noCNA=round(cn_sample["noCNA"][[1]]/genome_len, 3), 
               clonal=round(cn_sample["clonal"][[1]]/genome_len, 3), 
               subclonal=round(cn_sample["subclonal"][[1]]/genome_len, 3), 
-              ploidy_category=ploid))
+              ploidy_category=ploid,
+              has_clonal_cna=has_clonal_cna,
+              has_clonal_cna_medium=has_clonal_cna_medium,
+              has_clonal_cna_large=has_clonal_cna_large))
 }
 print("Copy number")
 res = as.data.frame(matrix(unlist(
   lapply(samplelist, function(x, ploidy) { getCNAFractions(x, output) })), 
-  ncol=4, byrow=T))
-colnames(res) = c("noCNA", "clonal", "subclonal", "ploidy_category")
-output = data.frame(output, res)
+  ncol=7, byrow=T))
+colnames(res) = c("noCNA", "clonal", "subclonal", "ploidy_category", "has_clonal_cna", "has_clonal_cna_medium", "has_clonal_cna_large")
+# Only add the required columns here
+output = data.frame(output, res[, !grepl("has_clonal", colnames(res))])
+
+# Save these for adding later
+clonal_cna_inventory = res[, grepl("has_clonal", colnames(res))]
 
 #############################################################################################################################
 # coverage 
@@ -225,7 +261,7 @@ for (infile in GENDER_FILES) {
 }
 print("Sex")
 row_match = match(output$samplename, sex$tumour)
-output$sex = sex[row_match,]$pred_gender
+output$inferred_sex = sex[row_match,]$pred_gender
 
 #############################################################################################################################
 # power calculation
@@ -246,6 +282,7 @@ output$refit = NA
 #############################################################################################################################
 # Annotations from ICGC, ids and histology
 #############################################################################################################################
+print("ICGC Annotations")
 anno = readr::read_tsv(ICGC_HISTOLOGY_FILE)
 annotations = lapply(1:nrow(sampleinput), function(i) {
   sel = sampleinput$icgc_sample_id[i]==anno$icgc_sample_id & sampleinput$icgc_donor_id[i]==anno$icgc_donor_id & anno$specimen_library_strategy=="WGS"
@@ -256,6 +293,33 @@ annotations = lapply(1:nrow(sampleinput), function(i) {
   }})
 annotations = do.call(rbind, annotations)
 output = data.frame(output, annotations)
+
+anno = readr::read_tsv(ICGC_CLINICAL_FILE)
+annotations = lapply(1:nrow(sampleinput), function(i) {
+  sel = sampleinput$icgc_donor_id[i]==anno$icgc_donor_id
+  if (sum(sel) > 0) {
+    anno[sel, c("donor_sex")]
+  } else {
+    data.frame(donor_sex=NA)
+  }})
+annotations = do.call(rbind, annotations)
+colnames(annotations) = c("reported_sex")
+output = data.frame(output, annotations)
+
+#############################################################################################################################
+# Add cluster details, unraveled earlier when collecting the cluster stats
+#############################################################################################################################
+print("Cluster details")
+res = lapply(clust_details, function(x) data.frame(paste(round(x[[1]], 4), collapse=";"), paste(x[[2]], collapse=";")))
+res = do.call(rbind, res)
+colnames(res) = c("cluster_locations", "cluster_sizes")
+output = data.frame(output, res)
+
+#############################################################################################################################
+# Add clonal CNA inventory
+#############################################################################################################################
+print("Clonal CNA inventory")
+output = data.frame(output, clonal_cna_inventory)
 
 #############################################################################################################################
 # save output
